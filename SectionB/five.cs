36875e0f7dc0d7eb1eb2e2a9e34af7dbc201bfff
jaisree <jaisree.anandhakumar@psiog.com>
2025-10-02 09:46:52 +0530

    predir

--- SectionB/five.cs
+++ SectionB/five.cs
@@ -1,35 +1,40 @@
-// #define DEBUG
-#undef TEST
-
 using System;
+//constructor chaining
+class Student
+{
+    public string Name;
+    public int Age;
+    public string Course;
+    public Student() : this("Unknown")   
+    {
+        Console.WriteLine("Constructor1");
+    }
+    public Student(string name) : this(name, 18, "Not Assigned")   
+    {
+        Console.WriteLine("Constructor2");
+    }
+    public Student(string name, int age, string course)
+    {
+        Name = name;
+        Age = age;
+        Course = course;
+        Console.WriteLine("Constructor3");
+    }
+    public void Show()
+    {
+        Console.WriteLine($"Name:{Name},Age:{Age},Course:{Course}");
+    }
+}
 
 class Program
 {
     static void Main()
     {
-#if DEBUG
-        Console.WriteLine("Debug");
-#endif
-
-        #region Greeting Methods
-        a();
-        b();
-        #endregion
-
-#pragma warning disable CS0168
-        int unusedvar;
-#pragma warning restore CS0168
+        Student s1 = new Student();   
+        Student s2 = new Student("Sree");   
+        Student s3 = new Student("Jai", 21, "C#");   
+        s1.Show();
+        s2.Show();
+        s3.Show();
     }
-
-    #region Methods
-    static void a()
-    {
-        Console.WriteLine("hello");
-    }
-
-    static void b()
-    {
-        Console.WriteLine("hai");
-    }
-    #endregion
 }
