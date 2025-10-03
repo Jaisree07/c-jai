f615673464782b253aded8acae226721a6cc6a9f
jaisree <jaisree.anandhakumar@psiog.com>
2025-10-03 17:29:46 +0530

    events

---
+++ SectionD/events.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class AlarmClock
+{
+    public delegate void AlarmEventHandler(object sender, EventArgs e);
+    public event AlarmEventHandler Ring;
+    public void Start()
+    {
+        Console.WriteLine("AlarmClock: Time reached! Ringing...");
+        Ring?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public void OnAlarmRing(object sender, EventArgs e)
+    {
+        Console.WriteLine($"{Name}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        // Publisher
+        AlarmClock alarm = new AlarmClock();
+        // Subscribers
+        Person alice = new Person("Jai");
+        Person bob = new Person("sree");
+        // Subscribe to the event
+        alarm.Ring += alice.OnAlarmRing;
+        alarm.Ring += bob.OnAlarmRing;
+        // Start the alarm → triggers event → notifies subscribers
+        alarm.Start();
+    }
+}
