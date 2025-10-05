76d7ef64e9bff467099025d522eb4765cd8e58b6
jaisree <jaisree.anandhakumar@psiog.com>
2025-10-05 12:54:47 +0530

    threadlogger

---
+++ SectionG/threadlogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public sealed class Logger
+{
+    private static readonly Logger _instance = new Logger();
+    private Logger() { }
+    public static Logger Instance => _instance;
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log:{message}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Accessing singleton instance
+        Logger.Instance.Log("This is a thread-safe log message");
+        Logger.Instance.Log("Same instance used again");
+    }
+}
